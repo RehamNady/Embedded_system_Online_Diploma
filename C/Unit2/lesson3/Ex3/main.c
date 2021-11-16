e398a00ddfe7681d4d15122c93e0b04684578676
Reham Nady <reham.nady68@gmail.com>
2021-11-16 02:20:13 +0200

    Add Two Integers

---
+++ C/Unit2/lesson3/Ex3/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 16, 2021
+ *      Author: reham
+ *      Add Two Integers
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+	int num1,num2;
+	printf("Enter two integers:");
+	fflush(stdin);
+	fflush(stdout);
+	scanf("%d",&num1);
+	scanf("%d",&num2);
+	printf("Sum: %d",num1+num2);
+
+
+}
