215bfd00eaafaa5236971181fe21cbbe346ca185
Reham Nady <reham.nady68@gmail.com>
2021-11-16 20:20:31 +0200

    Check alphabets is vowel or constant

---
+++ C/Unit2/Lecture_3(C_Basic)/HW2/Ex3/main.c
@@ -0,0 +1,34 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 16, 2021
+ *      Author: reham
+ *      Find the largest number among three numbers
+ */
+
+#include <stdio.h>
+int main(){
+	float num1,num2,num3;
+	printf("Enter three numbers:");
+	fflush(stdin);fflush(stdout);
+	scanf("%f %f %f",&num1,&num2,&num3);
+	if(num1 > num2){
+		if(num1>num3){
+			printf("Largest number =%f",num1);
+		}
+		else{
+			printf("Largest number =%f",num3);
+		}
+	}
+	else{
+		if(num2>num3){
+			printf("Largest number =%f",num2);
+		}
+		else{
+			printf("Largest number =%f",num3);
+		}
+
+	}
+
+	return 0;
+}
