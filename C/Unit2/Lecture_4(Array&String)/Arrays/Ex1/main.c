9ea03db5ad17c926a03faf2504a1daf2e07db99a
Reham Nady <reham.nady68@gmail.com>
2021-11-22 22:39:37 +0200

    sum of 2 matrix of order 2*2

--- C/Unit2/Lecture_4(Array&String)/Arrays/Ex1/main.c
+++ C/Unit2/Lecture_4(Array&String)/Arrays/Ex1/main.c
@@ -11,24 +11,22 @@ int main(){
 	int i,j;
 	float a[2][2],sum[2][2];
 	float b[2][2];
-	for(i=0;i<4;i++){
+	printf("Enter the elements of 1st matrix \n");
+	for(i=0;i<2;i++){
 		for(j=0;j<2;j++){
-			if(i<2){
-				if((i==0)&&(j==0)){
-					printf("Enter the elements of 1st matrix \n");
-				}
-				printf("Enter a%d%d",i+1,j+1);
-				fflush(stdin);fflush(stdout);
-				scanf("%f",&a[i][j]);
-			}
-			else{
-				if((i==2)&&(j==0)){
-					printf("Enter the elements of 2nd matrix \n");
-				}
-				printf("Enter b%d%d",i-1,j+1);
-				fflush(stdin);fflush(stdout);
-				scanf("%f",&b[i-2][j]);
-			}
+
+			printf("Enter a%d%d: ",i+1,j+1);
+			fflush(stdin);fflush(stdout);
+			scanf("%f",&a[i][j]);
+		}
+	}
+	printf("Enter the elements of 2nd matrix \n");
+	for(i=0;i<2;i++){
+		for(j=0;j<2;j++){
+
+			printf("Enter b%d%d: ",i+1,j+1);
+			fflush(stdin);fflush(stdout);
+			scanf("%f",&b[i][j]);
 		}
 	}
 	printf("Sum of Matrix:\n");
